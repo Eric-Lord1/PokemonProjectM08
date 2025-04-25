c6f07ebb5317a6fc9c26fdcb10210e30f73dc048
alex <alexvictorb@gmail.com>
2025-04-25 16:41:54 +0200

    pantalla inicial hecha

--- core/src/main/java/poke/iticbcn/alex_and_eric/MainScreen.java
+++ core/src/main/java/poke/iticbcn/alex_and_eric/MainScreen.java
@@ -1,34 +1,26 @@
 package poke.iticbcn.alex_and_eric;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class MainScreen extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture image;
+import poke.iticbcn.alex_and_eric.screens.SeleccionScreen;
 
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class MainScreen extends Game {
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
-    }
-
-    @Override
-    public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 140, 210);
-        batch.end();
+        //AssetManager.load();
+        setScreen(new SeleccionScreen());
     }
 
     @Override
     public void dispose() {
-        batch.dispose();
-        image.dispose();
+        super.dispose();
+        //AssetManager.dispose();
     }
 }
