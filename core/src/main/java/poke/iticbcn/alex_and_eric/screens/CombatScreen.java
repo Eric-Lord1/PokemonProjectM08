902d38da2e0c8e02b4c7cba7cd42bb57035f2845
alex <alexvictorb@gmail.com>
2025-04-29 19:36:12 +0200

    pantalla combate creada

---
+++ core/src/main/java/poke/iticbcn/alex_and_eric/screens/CombatScreen.java
@@ -0,0 +1,61 @@
+package poke.iticbcn.alex_and_eric.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.Batch;
+
+public class CombatScreen implements Screen {
+
+    public static Music music;
+    private Batch batch;
+    private OrthographicCamera camera;
+
+
+    public CombatScreen(){
+
+        music = Gdx.audio.newMusic(Gdx.files.internal("sounds/tema_principal.mp3"));
+        music.setVolume(0.2f);
+        music.setLooping(true);
+        music.play();
+
+
+    }
+
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
